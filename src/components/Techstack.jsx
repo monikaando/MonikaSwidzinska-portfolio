eecfde61b5359aeecc6121c55e443bd9a49f21c8
Monika <mswidzinska87@gmail.com>
2020-04-05 20:13:12 +0200

    test

--- src/components/Techstack.jsx
+++ src/components/Techstack.jsx
@@ -15,6 +15,7 @@ const Techstack = () => {
             <img src="https://res.cloudinary.com/mokaweb/image/upload/v1586101241/mongodb.png" alt="MongoDB"/>
             <img src="https://res.cloudinary.com/mokaweb/image/upload/v1586101240/github.png" alt="GitHub"/>
             <img src="https://res.cloudinary.com/mokaweb/image/upload/v1586101241/postman.png" alt="Postman"/>
+            <img src="" alt=""/>
         </div>
     );
 }
