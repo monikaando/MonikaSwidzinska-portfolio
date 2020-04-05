188156dca1e5c63e4f3b821a3d96f9c6ae6c45d9
Monika <mswidzinska87@gmail.com>
2020-04-05 20:11:32 +0200

    images on claudinary, added icons

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import React from 'react';
 import './App.css';
 import Navigation from "./components/Navigation"
-import Logo from "./components/Logo"
+import Header from "./components/Header"
+import Techstack from "./components/Techstack"
+
 // import {Switch, Route} from 'react-router-dom';
 
 
 function App() {
   return (
-    <div className="hero">
-     <Navigation/>
-     <Logo/>
-     {/* <Techstack/> */}
-     {/* <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/projects" component={Projects}/>
-        <Route path="/cv" component={Cv}/>
-         <Route path="/contact" component={Contact}/>
-     </Switch> */}
+    <div>
+        <div className="hero">
+            <Navigation/>
+            <Header/>
+        </div>
+           <Techstack/>
     </div>
   );
 }
---
+++ src/components/Techstack.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import "./Techstack.scss"
+const Techstack = () => {
+    return (
+        <div className="icons">
+            <img src="https://res.cloudinary.com/mokaweb/image/upload/v1586101240/html5.png" alt="HTML5"/>
+            <img src="https://res.cloudinary.com/mokaweb/image/upload/v1586101240/css3.png" alt="CSS3"/>
+            <img src="https://res.cloudinary.com/mokaweb/image/upload/v1586101241/javascript.png" alt="JavaScript"/>
+            <img src="https://res.cloudinary.com/mokaweb/image/upload/v1586101241/sass.png" alt="Sass"/>
+            <img src="https://res.cloudinary.com/mokaweb/image/upload/v1586101240/bootstrap.png" alt="Bootstrap"/>
+            <img src="https://res.cloudinary.com/mokaweb/image/upload/v1586101240/bulma.png" alt="Bulma"/>
+            <img src="https://res.cloudinary.com/mokaweb/image/upload/v1586109842/react_logo.png" alt="React"/>
+            <img src="https://res.cloudinary.com/mokaweb/image/upload/v1586101241/nodejs.png" alt="NodeJS"/>
+            <img src="https://res.cloudinary.com/mokaweb/image/upload/v1586101240/expressjs.png" alt="ExpressJS"/>
+            <img src="https://res.cloudinary.com/mokaweb/image/upload/v1586101241/mongodb.png" alt="MongoDB"/>
+            <img src="https://res.cloudinary.com/mokaweb/image/upload/v1586101240/github.png" alt="GitHub"/>
+            <img src="https://res.cloudinary.com/mokaweb/image/upload/v1586101241/postman.png" alt="Postman"/>
+        </div>
+    );
+}
+
+export default Techstack;
