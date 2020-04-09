d01476315930406fb4b49314cd14012bdb737704
Monika <mswidzinska87@gmail.com>
2020-04-09 17:40:18 +0200

    social media icons

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -17,6 +17,21 @@ const Navigation = () => {
                     <li><NavLink to ="/projects">Projects</NavLink></li>
                      <li><NavLink to ="/cv">CV</NavLink></li>
                     <li><NavLink to ="/contact">Contact</NavLink></li>
+                    <li>
+                        <a href="https://github.com/mswidzinska">
+                             <img src="https://res.cloudinary.com/mokaweb/image/upload/v1586445839/MokaWeb-images/github.png" alt="Monika-Swidzinska-github"/>
+                        </a>
+                    </li>
+                    <li>
+                        <NavLink to="https://www.linkedin.com/in/monikaswidzinska/">
+                            <img src="https://res.cloudinary.com/mokaweb/image/upload/v1586445839/MokaWeb-images/linkedin.png" alt="Monika-Swidzinska-linkedin"/>
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="mailto:mswidzinska87@gmail.com">
+                            <img src="https://res.cloudinary.com/mokaweb/image/upload/v1586445839/MokaWeb-images/email.png" alt="MokaWeb-email"/>
+                        </NavLink>
+                    </li>
                 </ul>
             </div>
         </div>
--- src/components/Techstack.jsx
+++ src/components/Techstack.jsx
@@ -3,18 +3,18 @@ import "./Techstack.scss"
 const Techstack = () => {
     return (
         <div className="icons">
-            <img src="https://res.cloudinary.com/mokaweb/image/upload/v1586101240/html5.png" alt="HTML5"/>
-            <img src="https://res.cloudinary.com/mokaweb/image/upload/v1586101240/css3.png" alt="CSS3"/>
-            <img src="https://res.cloudinary.com/mokaweb/image/upload/v1586101241/javascript.png" alt="JavaScript"/>
-            <img src="https://res.cloudinary.com/mokaweb/image/upload/v1586101241/sass.png" alt="Sass"/>
-            <img src="https://res.cloudinary.com/mokaweb/image/upload/v1586101240/bootstrap.png" alt="Bootstrap"/>
-            <img src="https://res.cloudinary.com/mokaweb/image/upload/v1586101240/bulma.png" alt="Bulma"/>
-            <img src="https://res.cloudinary.com/mokaweb/image/upload/v1586109842/react_logo.png" alt="React"/>
-            <img src="https://res.cloudinary.com/mokaweb/image/upload/v1586101241/nodejs.png" alt="NodeJS"/>
-            <img src="https://res.cloudinary.com/mokaweb/image/upload/v1586101240/expressjs.png" alt="ExpressJS"/>
-            <img src="https://res.cloudinary.com/mokaweb/image/upload/v1586101241/mongodb.png" alt="MongoDB"/>
-            <img src="https://res.cloudinary.com/mokaweb/image/upload/v1586101240/github.png" alt="GitHub"/>
-            <img src="https://res.cloudinary.com/mokaweb/image/upload/v1586101241/postman.png" alt="Postman"/>
+            <img src="https://res.cloudinary.com/mokaweb/image/upload/v1586101240/MokaWeb-icons/html5.png" alt="HTML5"/>
+            <img src="https://res.cloudinary.com/mokaweb/image/upload/v1586101240/MokaWeb-icons/css3.png" alt="CSS3"/>
+            <img src="https://res.cloudinary.com/mokaweb/image/upload/v1586101241/MokaWeb-icons/javascript.png" alt="JavaScript"/>
+            <img src="https://res.cloudinary.com/mokaweb/image/upload/v1586101241/MokaWeb-icons/sass.png" alt="Sass"/>
+            <img src="https://res.cloudinary.com/mokaweb/image/upload/v1586101240/MokaWeb-icons/bootstrap.png" alt="Bootstrap"/>
+            <img src="https://res.cloudinary.com/mokaweb/image/upload/v1586101240/MokaWeb-icons/bulma.png" alt="Bulma"/>
+            <img src="https://res.cloudinary.com/mokaweb/image/upload/v1586109842/MokaWeb-icons/react_logo.png" alt="React"/>
+            <img src="https://res.cloudinary.com/mokaweb/image/upload/v1586101241/MokaWeb-icons/nodejs.png" alt="NodeJS"/>
+            <img src="https://res.cloudinary.com/mokaweb/image/upload/v1586435023/MokaWeb-icons/expressJS.png" alt="ExpressJS"/>
+            <img src="https://res.cloudinary.com/mokaweb/image/upload/v1586101241/MokaWeb-icons/mongodb.png" alt="MongoDB"/>
+            <img src="https://res.cloudinary.com/mokaweb/image/upload/v1586101240/MokaWeb-icons/github.png" alt="GitHub"/>
+            <img src="https://res.cloudinary.com/mokaweb/image/upload/v1586101241/MokaWeb-icons/postman.png" alt="Postman"/>
         </div>
     );
 }
