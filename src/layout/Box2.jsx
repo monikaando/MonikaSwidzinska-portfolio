6a52f4ce27ac81cd543a612ad2fe706c2fb377ca
Monika <mswidzinska87@gmail.com>
2020-05-25 12:53:41 +0200

    responsivity

--- src/layout/Box2.jsx
+++ src/layout/Box2.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import "../styles/Box2.scss";
-import Buttons from "../components/Buttons";
 
 const Box2 = (props) => {
   return (
     <div>
       <div className="box2">{props.children}</div>
-      <Buttons />
     </div>
   );
 };
