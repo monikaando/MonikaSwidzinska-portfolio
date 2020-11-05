4305e6092c061c2e9faf2cf083225e501ecc5cb0
Monika <mswidzinska87@gmail.com>
2020-11-05 17:08:51 +0100

    New projects

--- src/thirdParty/YouTube.jsx
+++ src/thirdParty/YouTube.jsx
@@ -1,22 +1,20 @@
-import React from "react";
-import YouTube from "react-youtube";
+import React from 'react';
+import YouTube from 'react-youtube';
 
 class YouTubeVideo extends React.Component {
-  videoOnReady(event) {
-    event.target.stopVideo();
-  }
-  render() {
-    const opts = {
-      width: "400px",
-      height: "340px",
-      playerVars: {
-        autoplay: 1,
-      },
-    };
-    const { videoId } = this.props;
-    return (
-      <YouTube videoId={videoId} opts={opts} onReady={this.videoOnReady} />
-    );
-  }
+	videoOnReady(event) {
+		event.target.stopVideo();
+	}
+	render() {
+		const opts = {
+			width: '500px',
+			height: '340px',
+			playerVars: {
+				autoplay: 1,
+			},
+		};
+		const { videoId } = this.props;
+		return <YouTube videoId={videoId} opts={opts} onReady={this.videoOnReady} />;
+	}
 }
 export default YouTubeVideo;
