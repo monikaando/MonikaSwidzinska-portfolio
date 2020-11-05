c909f77457acc6b9abf2455ccab632aa7c4d6a11
Monika <mswidzinska87@gmail.com>
2020-11-05 17:12:38 +0100

    update

--- src/components/Techstack.jsx
+++ src/components/Techstack.jsx
@@ -1,92 +1,92 @@
-import React from "react";
-import "../styles/Techstack.scss";
+import React from 'react';
+import '../styles/Techstack.scss';
 const Techstack = () => {
-  return (
-    <div className="tech row">
-      <div className="box col-md-5 offset-md-1 m-bottom">
-        <h3>Main skills</h3>
-        <div className="icons pr-md-5">
-          <img
-            src="https://res.cloudinary.com/mokaweb/image/upload/v1601318186/MokaWeb-icons/AngularJS.png"
-            alt="AngularJS"
-          />
-          <img
-            src="https://res.cloudinary.com/mokaweb/image/upload/v1586109842/MokaWeb-icons/react_logo.png"
-            alt="React"
-          />
-          <img
-            src="https://res.cloudinary.com/mokaweb/image/upload/v1601318186/MokaWeb-icons/ionic.png"
-            alt="IONIC"
-          />
-          <img
-            src="https://res.cloudinary.com/mokaweb/image/upload/v1601318186/MokaWeb-icons/TypeScript.png"
-            alt="TypeScript"
-          />
-          <img
-            src="https://res.cloudinary.com/mokaweb/image/upload/v1586101241/MokaWeb-icons/javascript.png"
-            alt="JavaScript"
-          />
-          <img
-            src="https://res.cloudinary.com/mokaweb/image/upload/v1586101241/MokaWeb-icons/sass.png"
-            alt="Sass"
-          />
-            <img
-            src="https://res.cloudinary.com/mokaweb/image/upload/v1586101240/MokaWeb-icons/css3.png"
-            alt="CSS3"
-          />
-          <img
-            src="https://res.cloudinary.com/mokaweb/image/upload/v1586101240/MokaWeb-icons/html5.png"
-            alt="HTML5"
-          />
-        </div>
-      </div>
-      <div className="box col-md-5 offset-md-1">
-        <h3>Other skills</h3>
-        <div className="col icons pr-md-5">
-          <img
-            src="https://res.cloudinary.com/mokaweb/image/upload/v1586101241/MokaWeb-icons/nodejs.png"
-            alt="NodeJS"
-          />
-          <img
-            src="https://res.cloudinary.com/mokaweb/image/upload/v1586435023/MokaWeb-icons/expressJS.png"
-            alt="ExpressJS"
-          />
-          <img
-            src="https://res.cloudinary.com/mokaweb/image/upload/v1586101241/MokaWeb-icons/mongodb.png"
-            alt="MongoDB"
-          />
-          <img
-            src="https://res.cloudinary.com/mokaweb/image/upload/v1586101240/MokaWeb-icons/github.png"
-            alt="GitHub"
-          />
-          <img
-            src="https://res.cloudinary.com/mokaweb/image/upload/v1601318978/MokaWeb-icons/GitLab.png"
-            alt="GitLab"
-          />
-          <img
-            src="https://res.cloudinary.com/mokaweb/image/upload/v1602775365/MokaWeb-icons/GoJs.png"
-            alt="GoJS"
-          />
-          <img
-            src="https://res.cloudinary.com/mokaweb/image/upload/v1601318186/MokaWeb-icons/RxJS.png"
-            alt="RxJS"
-          />
-           <img
-            src="https://res.cloudinary.com/mokaweb/image/upload/v1586101240/MokaWeb-icons/bootstrap.png"
-            alt="Bootstrap"
-          />
-          {/* <img
+	return (
+		<div className="tech row">
+			<div className="box col-md-5 offset-md-1 m-bottom">
+				<h3>Main skills</h3>
+				<div className="icons pr-md-5">
+					<img
+						src="https://res.cloudinary.com/mokaweb/image/upload/v1586109842/MokaWeb-icons/react_logo.png"
+						alt="React"
+					/>
+					<img
+						src="https://res.cloudinary.com/mokaweb/image/upload/v1601318186/MokaWeb-icons/AngularJS.png"
+						alt="AngularJS"
+					/>
+					<img
+						src="https://res.cloudinary.com/mokaweb/image/upload/v1601318186/MokaWeb-icons/ionic.png"
+						alt="IONIC"
+					/>
+					<img
+						src="https://res.cloudinary.com/mokaweb/image/upload/v1601318186/MokaWeb-icons/TypeScript.png"
+						alt="TypeScript"
+					/>
+					<img
+						src="https://res.cloudinary.com/mokaweb/image/upload/v1586101241/MokaWeb-icons/javascript.png"
+						alt="JavaScript"
+					/>
+					<img
+						src="https://res.cloudinary.com/mokaweb/image/upload/v1586101241/MokaWeb-icons/sass.png"
+						alt="Sass"
+					/>
+					<img
+						src="https://res.cloudinary.com/mokaweb/image/upload/v1586101240/MokaWeb-icons/css3.png"
+						alt="CSS3"
+					/>
+					<img
+						src="https://res.cloudinary.com/mokaweb/image/upload/v1586101240/MokaWeb-icons/html5.png"
+						alt="HTML5"
+					/>
+				</div>
+			</div>
+			<div className="box col-md-5 offset-md-1">
+				<h3>Other skills</h3>
+				<div className="col icons pr-md-5">
+					<img
+						src="https://res.cloudinary.com/mokaweb/image/upload/v1586101241/MokaWeb-icons/nodejs.png"
+						alt="NodeJS"
+					/>
+					<img
+						src="https://res.cloudinary.com/mokaweb/image/upload/v1586435023/MokaWeb-icons/expressJS.png"
+						alt="ExpressJS"
+					/>
+					<img
+						src="https://res.cloudinary.com/mokaweb/image/upload/v1586101241/MokaWeb-icons/mongodb.png"
+						alt="MongoDB"
+					/>
+					<img
+						src="https://res.cloudinary.com/mokaweb/image/upload/v1586101240/MokaWeb-icons/github.png"
+						alt="GitHub"
+					/>
+					<img
+						src="https://res.cloudinary.com/mokaweb/image/upload/v1601318978/MokaWeb-icons/GitLab.png"
+						alt="GitLab"
+					/>
+					<img
+						src="https://res.cloudinary.com/mokaweb/image/upload/v1602775365/MokaWeb-icons/GoJs.png"
+						alt="GoJS"
+					/>
+					<img
+						src="https://res.cloudinary.com/mokaweb/image/upload/v1601318186/MokaWeb-icons/RxJS.png"
+						alt="RxJS"
+					/>
+					<img
+						src="https://res.cloudinary.com/mokaweb/image/upload/v1586101240/MokaWeb-icons/bootstrap.png"
+						alt="Bootstrap"
+					/>
+					{/* <img
             src="https://res.cloudinary.com/mokaweb/image/upload/v1586101240/MokaWeb-icons/bulma.png"
             alt="Bulma"
           /> */}
-          <img
-            src="https://res.cloudinary.com/mokaweb/image/upload/v1601318186/MokaWeb-icons/Jasmine-testing.png"
-            alt="Jasmine"
-          />
-        </div>
-    </div>
-  </div>
-  );
+					<img
+						src="https://res.cloudinary.com/mokaweb/image/upload/v1601318186/MokaWeb-icons/Jasmine-testing.png"
+						alt="Jasmine"
+					/>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Techstack;
