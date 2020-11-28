aeaf780e86bd410bf4edfc91329026557eee95ac
Monika <mswidzinska87@gmail.com>
2020-11-28 12:59:22 +0100

    fixed yt

--- src/thirdParty/YouTube.jsx
+++ src/thirdParty/YouTube.jsx
@@ -12,7 +12,7 @@ class YouTubeVideo extends React.Component {
 			playerVars: {
 				autoplay: 1,
 			},
-			host: 'https://www.youtube.com',
+			host: 'http://www.youtube.com',
 		};
 		const { videoId } = this.props;
 		return <YouTube videoId={videoId} opts={opts} onReady={this.videoOnReady} />;
